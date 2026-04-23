95805b03dfffb0ebba729950aeb6b9cab1ea2311
shironeko <mkmknknk@gmail.com>
2026-04-23 15:08:33 +0900

    初回公開

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'しろねこ羅針盤';
+export const SITE_DESCRIPTION = '夫婦・家庭の悩みを占いでナビゲート。白猫があなたの道を照らします。';
